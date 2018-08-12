789963d072da013464f95133a12c5058b39a8b99
Jessop Breth <jessopb@gmail.com>
2018-08-11 23:30:55 -0400

    Adds painting capability and algorithm timing on 1000x1000 image.

--- find_largest_region.py
+++ find_largest_region.py
@@ -1,11 +1,16 @@
 from PIL import Image
 
 
-def find_largest_region(image_file):
-    '''Returns the size of the largest contiguous color region in an image_file
+def find_largest_region(image_file, paint_new):
+    '''Returns the size of the largest contiguous color region in an image.
+       If paint_new is true, it saves a new image with that region painted.
     '''
-    largest_region = 0
+
+    largest_region = []
+    largest_region.append([0])
+    paint = paint_new
     global_px_visited = set()
+
     img = Image.open(image_file)
     xmax = img.size[0] - 1
     ymax = img.size[1] - 1
@@ -41,23 +46,33 @@ def find_largest_region(image_file):
         # to output more comprehensive region data,
         # append region_px_visited set to list from here
 
-        return len(region_px_visited)
+        return region_px_visited
 
     for y in range(img.size[1]):
         for x in range(img.size[0]):
             coord = (x, y)
             # global_px_visited says whether to bother with a pixel globally
             if coord not in global_px_visited:
-                size = flood_region(coord)
-                if size > largest_region:
-                    largest_region = size
+                region_pixels = flood_region(coord)
+                if len(region_pixels) > len(largest_region[0]):
+                    largest_region[0] = region_pixels
 
-    return largest_region
+    if paint is True:
+        for point in largest_region[0]:
+            img.putpixel((point), (255, 0, 0))
+        img.save('newmap.bmp')
+    print(largest_region[0])
+    return len(largest_region[0])
 
 
 def test():
-    print("Largest Region has", find_largest_region('map1.bmp'), 'pixels.')
+    print('Largest Region has',
+          find_largest_region('map1000.bmp', True),
+          'pixels.')
 
 
 if __name__ == '__main__':
+    import time
+    t = time.time()
     test()
+    print('Time was', time.time() - t)
